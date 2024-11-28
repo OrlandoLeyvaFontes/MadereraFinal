2b6df0e64f820787c4ef180db3e9cad531450b96
OrlandoLeyvaFontes <orlando.leyva245512@potros.itson.edu.mx>
2024-11-28 11:28:49 -0700

    Subsistema de lo que me pidio el maestro

---
+++ ObtenerVentasSS/src/main/java/interfaz/IObtenerVentasSS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaz;
+
+import dto.VentaDTO;
+import java.util.List;
+
+/**
+ *
+ * @author Oley
+ */
+public interface IObtenerVentasSS {
+         public List<VentaDTO> obtenerVentas();
+
+}
